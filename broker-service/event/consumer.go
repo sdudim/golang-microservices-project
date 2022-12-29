62cea2c281bfd8af6ad6730778b012973ca2c5c0
sdudim <david@specula.io>
2022-12-29 23:14:32 +0200

    29122022

---
+++ broker-service/event/consumer.go
@@ -0,0 +1,143 @@
+package event
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	ampq "github.com/rabbitmq/amqp091-go"
+)
+
+type Consumer struct {
+	conn      *ampq.Connection
+	queueName string
+}
+
+func NewConsumer(conn *ampq.Connection) (Consumer, error) {
+
+	consumer := Consumer{
+		conn: conn,
+	}
+
+	err := consumer.setup()
+	if err != nil {
+		return Consumer{}, err
+	}
+
+	return consumer, nil
+
+}
+
+func (consumer *Consumer) setup() error {
+	channel, err := consumer.conn.Channel()
+	if err != nil {
+		return err
+	}
+
+	return declareExchange(channel) // this function declared Exchange on the AMPQ server (the fuction declaration is in evnet.go file)
+}
+
+type Payload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
+}
+
+func (consumer *Consumer) Listen(topics []string) error {
+	ch, err := consumer.conn.Channel()
+	if err != nil {
+		return err
+	}
+	defer ch.Close()
+
+	q, err := declareRandomQueue(ch)
+	if err != nil {
+		return err
+	}
+
+	for _, s := range topics {
+		err = ch.QueueBind(
+			q.Name,
+			s,            // topic name
+			"logs_topic", // Exchange name
+			false,
+			nil,
+		)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	messages, err := ch.Consume(q.Name, "", true, false, false, false, nil)
+	if err != nil {
+		return err
+	}
+
+	forever := make(chan bool)
+	go func() {
+		for d := range messages {
+			var payload Payload
+			_ = json.Unmarshal(d.Body, &payload)
+
+			go handlePayload(payload)
+
+		}
+	}()
+
+	fmt.Printf("Waiting for message [Exchange, Queue] [logs_topic, %s]\n", q.Name)
+	<-forever
+	return nil
+}
+
+func handlePayload(payload Payload) {
+	switch payload.Name {
+	case "log", "event":
+		//log what ever we get
+		err := logEvent(payload)
+		if err != nil {
+			log.Println(err)
+		}
+	case "auth":
+		//authenticate
+
+	default:
+		err := logEvent(payload)
+		if err != nil {
+			log.Println(err)
+		}
+
+	}
+
+}
+
+func logEvent(entry Payload) error {
+	// create some json we'll send to the log microservice (In production we'll use marshal and not marshalIndent it more useful)
+	jsonData, _ := json.MarshalIndent(entry, "", "\t")
+
+	logServiceURL := "http://logger-service/log"
+
+	//create request to the service
+	request, err := http.NewRequest("POST", logServiceURL, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return err
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+
+	response, err := client.Do(request)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusAccepted {
+		return err
+	}
+
+	return nil
+
+}
