de680fc00db3e3c4373188739c45afc7ae95324a
sdudim <david@specula.io>
2022-12-29 23:11:37 +0200

    29122022

---
+++ broker-service/event/emitter.go
@@ -0,0 +1,62 @@
+package event
+
+import (
+	"log"
+
+	ampq "github.com/rabbitmq/amqp091-go"
+)
+
+type Emitter struct {
+	connection ampq.Connection
+}
+
+func (e *Emitter) setup() error {
+	channel, err := e.connection.Channel()
+	if err != nil {
+		panic(err)
+	}
+
+	defer channel.Close()
+	return declareExchange(channel)
+}
+
+func (e *Emitter) push(event string, severity string) error {
+	channel, err := e.connection.Channel()
+	if err != nil {
+		panic(err)
+	}
+
+	defer channel.Close()
+
+	log.Println("Pushing to channetl")
+	err = channel.Publish(
+		"logs_topic",
+		severity,
+		false,
+		false,
+		ampq.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(event),
+		},
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return nil
+
+}
+
+func NewEventEmitter(conn ampq.Connection) (Emitter, error) {
+	emitter := Emitter{
+		connection: conn,
+	}
+
+	err := emitter.setup()
+	if err != nil {
+		return Emitter{}, err
+	}
+
+	return emitter, nil
+}
